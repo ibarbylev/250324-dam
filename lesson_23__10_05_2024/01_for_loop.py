14bb0d2b12444804d85aca010861a81c5d7cc0ac
ibarbylev <ibarbylev@gmail.com>
2024-05-10 10:18:54 +0300

    some fixes

---
+++ lesson_23__10_05_2024/01_for_loop.py
@@ -0,0 +1,16 @@
+# for loop
+
+for fruit in ["apple", "potato", "pinapple"]:
+    if fruit == "potato":
+        break
+    print(fruit)
+else:
+    print("end loop")
+
+
+# range
+
+print(list(range(10)))
+
+for i in range(1, 11, 2):
+    print(i)
