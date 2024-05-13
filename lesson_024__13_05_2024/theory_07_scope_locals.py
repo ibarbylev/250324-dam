b88ab546b6f0f02bd8718f9104980d2467465824
ibarbylev <ibarbylev@gmail.com>
2024-05-13 10:58:45 +0300

    some fixes

--- lesson_024__13_05_2024/theory_07_scope_locals.py
+++ lesson_024__13_05_2024/theory_07_scope_locals.py
@@ -3,13 +3,13 @@ count = 0
 
 def outer():
     count_outer = 1
-    print("inside the outer function", locals().keys())
 
     def inner():
         count_inner = 2
         print("inside the inner function", locals().keys())
 
     inner()
+    print("inside the outer function", locals().keys())
 
 
 outer()
