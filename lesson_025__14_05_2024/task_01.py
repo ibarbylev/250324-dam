62be33ec34b9892752651ac8b3f7b0990bf604cc
ibarbylev <ibarbylev@gmail.com>
2024-05-14 12:30:03 +0300

    some fixes

---
+++ lesson_025__14_05_2024/task_01.py
@@ -0,0 +1,20 @@
+"""Перепишите решение следующей задачи с использованием функции.
+У нас есть две логические переменные: isWeekday, isVacation (выходной день и каникулы).
+Они могут принимать разные значения, всего 4 комбинации:
+true - true, true - false, false - false, false - true.
+Есть правило: мы можем поспать, если день не рабочий или мы на каникулах.
+Напишите программу, которая в зависимости от значений двух переменных печатает, можем ли мы поспать или нет.
+То есть для значений isWeekday = False и isVacation = False программа должна печатать “можете поспать”.
+"""
+
+
+def can_you_sleep(is_weekday: bool, is_vacation: bool) -> None:
+    word = " НЕ"
+    print(f"можете{word} поспать")
+
+
+can_you_sleep(False, False)
+can_you_sleep(False, True)
+can_you_sleep(True, False)
+can_you_sleep(True, True)
+
