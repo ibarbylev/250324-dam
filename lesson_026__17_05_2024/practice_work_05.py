18375b6fe74bd438f7c5b33de5714742b91f6ccd
ibarbylev <ibarbylev@gmail.com>
2024-05-17 11:27:09 +0300

    some fixes

--- lesson_026__17_05_2024/practice_work_05.py
+++ lesson_026__17_05_2024/practice_work_05.py
@@ -4,4 +4,16 @@
 
 Последний элемент списка состоит из 26 символов z.
 """
+from pprint import pprint
 
+
+def func() -> None:
+    code_a = ord('a')  # 97
+    lst = []
+    for i in range(26):
+        lst.append(chr(code_a + i) * (i+1))
+
+    pprint(lst)
+
+
+func()
