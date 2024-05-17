65aabc152dc20743a1650cf53f7be8b80975c5ec
ibarbylev <ibarbylev@gmail.com>
2024-05-17 10:20:11 +0300

    some fixes

--- lesson_026__17_05_2024/practice_work_01.py
+++ lesson_026__17_05_2024/practice_work_01.py
@@ -7,3 +7,20 @@ result = 3
 """
 
 
+def count_vowels(word: str) -> int:
+    vowels = 'aeiouyAEIOUY'
+
+    # ============== variant 1 ====================
+    # count = 0
+    # for s in word:
+    #     if s in vowels:
+    #         count += 1
+    # return count
+
+    # ============== variant 2 ====================
+    return len([s for s in word if s in vowels])
+
+
+text = 'hEllO world'
+print(count_vowels(text))
+
