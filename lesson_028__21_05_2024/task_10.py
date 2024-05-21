23f93229d64753777ee0408ab86825fd9fed4811
ibarbylev <ibarbylev@gmail.com>
2024-05-21 14:25:21 +0300

    some fixes

--- lesson_028__21_05_2024/task_10.py
+++ lesson_028__21_05_2024/task_10.py
@@ -12,9 +12,17 @@ def count_uniq_items(nums: list[int]) -> int:
     # return len(set(nums))
 
     # ================ var 2  ===============
-    return 0
+    if not nums:
+        return 0
+    count = 1
+    for i in range(1, len(nums)):
+        if nums[i] != nums[i - 1]:
+            count += 1
+
+    return count
 
 
 print(count_uniq_items([1, 2, 2, 3, 3, 3]))  # 3
 print(count_uniq_items([1, 1, 1, 1, 1, 1]))  # 1
+print(count_uniq_items([1]))                 # 1
 print(count_uniq_items([]))                  # 0
