7b6332c51fc9e49c4d5a9751c5b802ae20ed74e4
ibarbylev <ibarbylev@gmail.com>
2024-05-24 09:10:52 +0300

    some fixes

--- lesson_030__23_05_2024/theory/theory_01_list_comprehension.py
+++ lesson_030__23_05_2024/theory/theory_01_list_comprehension.py
@@ -9,3 +9,15 @@ print(lst_1)
 lst_2 = [n for n in range(num)]
 print(lst_1)
 
+""" =========== if condition =========== """
+nums = list(range(10))
+print(nums)
+
+even_nums = [n for n in nums if n % 2 == 0]
+print(even_nums)
+
+
+""" =========== if else condition =========== """
+even_and_double_odd_nums = [n if n % 2 == 0 else n * 2 for n in nums]
+print(even_and_double_odd_nums)
+
--- lesson_030__23_05_2024/theory/theory_06_sort.py
+++ lesson_030__23_05_2024/theory/theory_06_sort.py
@@ -1,11 +1,11 @@
 # print(help(list.sort))
 
 def key_sort(x):
-    return x[1][1]
+    return x[1][0]
 
 
-my_list = [(2, 'cv'), (1, 'ax'), (2, 'cc')]
-my_list.sort(key=key_sort, reverse=True)
+my_list = [(3, 'cv'), (1, 'ax'), (2, 'cc')]
+my_list.sort(key=key_sort)
 # ['cv', 'd', 'cc']
 
 print(my_list)  # [(1, 'd'), (2, 'cv'), (2, 'cc')]
