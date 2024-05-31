b3a1efaf4525e20c68cb3ec85321a4b27da3fe4e
ibarbylev <ibarbylev@gmail.com>
2024-05-31 10:57:28 +0300

    some fixes

--- lesson_035__31_05_2024_Summary/task_05.py
+++ lesson_035__31_05_2024_Summary/task_05.py
@@ -1,17 +1,22 @@
 """Отсортируйте список по двум критериям:
    - по длине слова;
-   - по количеству гласных слове ('aeiouyAEIOUY').
+   - по длине слова и количеству гласных слове ('aeiouyAEIOUY').
+   - по длине слова и алфавиту.
    """
 
 
 words = ["apple", "orange", "banana", "grape", "peach", "melon", "berry", "plum", "kiwi"]
 
 print(f"Сортировка по длине слова: ")
-print(sorted(words, key=...))
-# ['plum', 'kiwi', 'apple', 'grape', 'peach', 'melon', 'berry', 'orange', 'banana']
+print(sorted(words, key=len))
+print(['plum', 'kiwi', 'apple', 'grape', 'peach', 'melon', 'berry', 'orange', 'banana'])
+
+print(f"Сортировка по длине слова  по количеству гласных слове ('aeiouyAEIOUY'): ")
+print(sorted(words, key=lambda word: (len(word), sum(1 for char in word if char in "aeiouyAEIOUY"))))
+print(['plum', 'kiwi', 'apple', 'grape', 'peach', 'melon', 'berry', 'orange', 'banana'])
 
 print(f"Сортировка по длине слова и алфавиту: ")
-print(sorted(words, key=...))
-# ['kiwi', 'plum', 'apple', 'berry', 'grape', 'melon', 'peach', 'banana', 'orange']
+print(sorted(words, key=lambda word: (len(word), word)))
+print(['kiwi', 'plum', 'apple', 'berry', 'grape', 'melon', 'peach', 'banana', 'orange'])
 
 
