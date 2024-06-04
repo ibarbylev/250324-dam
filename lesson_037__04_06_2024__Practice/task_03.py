6ecc8a78f21c904bf0a43e1819ac3f8743862fd9
ibarbylev <ibarbylev@gmail.com>
2024-06-04 11:15:09 +0300

    some fixes

---
+++ lesson_037__04_06_2024__Practice/task_03.py
@@ -0,0 +1,20 @@
+"""Дано натуральное число N.
+Написать функцию digits_sum(N), которая вычисляет сумму его цифр.
+При решении этой задачи пользуемся рекурсией, а строки, списки, массивы и циклы не используем.
+Пример digits_sum(179) = 17
+"""
+
+
+def digits_sum_non_tail(n):
+    pass
+
+
+print(digits_sum_non_tail(179))  # 17
+
+
+def digits_sum_tail(n, accumulator=0):
+    pass
+
+
+print(digits_sum_tail(179))  # 17
+
---
+++ lesson_037__04_06_2024__Practice/task_04.py
@@ -0,0 +1,22 @@
+"""Дано слово, состоящее только из строчных латинских букв.
+Проверьте с помощью рекурсии, является ли это слово палиндромом.
+Выведите YES или NO.
+
+Пример:
+    is_palindrome("radar") вернёт Yes
+    is_palindrome("abba")) вернёт No
+    is_palindrome("yes"))  вернёт No
+"""
+
+
+def is_palindrome(word):
+    if len(word) <= 1:
+        return 'Yes'
+    elif word[0] != word[-1]:
+        return 'No'
+    return is_palindrome(word[1:-1])
+
+
+print(is_palindrome("radar"))  # Yes
+print(is_palindrome("abba"))   # Yes
+print(is_palindrome("yes"))    # No
