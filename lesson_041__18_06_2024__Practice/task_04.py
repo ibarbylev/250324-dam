a80823ebcd7fe26db35e959a0446259c2b321e2e
ibarbylev <ibarbylev@gmail.com>
2024-06-18 15:05:23 +0300

    some fixes

--- lesson_041__18_06_2024__Practice/task_04.py
+++ lesson_041__18_06_2024__Practice/task_04.py
@@ -3,18 +3,30 @@
 """
 import json
 from pprint import pprint
+from typing import List, Dict
 
 
-def read_list_from_json_file(filename: str) -> dict[str, str]:
-    pass
+def read_list_from_json_file(filename: str) -> List[str]:
+    with open(filename, encoding='utf-8') as file:
+        return json.load(file)
 
 
 def write_to_json_file(filename: str, dct: dict[str, dict[str, int]]) -> None:
-    pass
+    with open(filename, 'w', encoding='utf-8') as file:
+        json.dump(dct, file, indent=4, ensure_ascii=False)
 
 
 def process_sales_data(read_from_file: str, write_to_file: str) -> None:
-    pass
+    initial_data: List = read_list_from_json_file(read_from_file)
+    sales = {}
+
+    for line in initial_data:
+        customer, product, quantity = line.split('-')
+        quantity = int(quantity)
+        customer_dict = sales.setdefault(customer, {})   # {'apple': 2, 'banana': 7}
+        sales[customer][product] = customer_dict.get(product, 0) + quantity
+
+    write_to_json_file(write_to_file, sales)
 
 
 process_sales_data('sales.json', 'sales-by-customers.json')
