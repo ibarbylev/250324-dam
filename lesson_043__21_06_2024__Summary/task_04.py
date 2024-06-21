056dbe6bd0920eee23a9f027239a775a26e2d77a
ibarbylev <ibarbylev@gmail.com>
2024-06-21 12:54:50 +0300

    some fixes

--- lesson_043__21_06_2024__Summary/task_04.py
+++ lesson_043__21_06_2024__Summary/task_04.py
@@ -9,6 +9,7 @@ from collections import Counter
 
 text = 'Coco Chanel: Je ne me soucie pas de ce que vous pensez de moi. Je ne pense pas à vous du tout.'
 # Здесь отсутствует одна строка кода:
+char_num = sorted(Counter(text).items(), key=lambda x: -x[1])
 print(*char_num[:3], sep="\n")
 
 
