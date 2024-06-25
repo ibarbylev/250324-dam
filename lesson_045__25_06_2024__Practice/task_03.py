ba34dce6e1a47c66ac2efa4b5764e68f2ffb9111
ibarbylev <ibarbylev@gmail.com>
2024-06-25 13:04:51 +0300

    some fixes

--- lesson_045__25_06_2024__Practice/task_03.py
+++ lesson_045__25_06_2024__Practice/task_03.py
@@ -23,16 +23,27 @@ PS
 from task_01 import users
 
 
+
 class IBANException(Exception):
     pass
 
 
 def iban_validation(iban: str) -> bool:
-    errors = []
-    "Incorrect Length of IBAN!"
-    "The first two characters must be letters!"
-    "The last twenty characters must be digits!"
-    IBANException('объединить все ошибка')
+    mistakes = []
+    iban_corr = iban.replace(" ", "")
+
+    if len(iban_corr) != 22:
+        mistakes.append("Incorrect Length of IBAN!")
+
+    if not iban_corr[:2].isalpha():
+        mistakes.append("The first two characters must be letters!")
+
+    if not iban_corr[2:].isdigit():
+        mistakes.append("The last twenty characters must be digits!")
+
+    if mistakes:
+        raise IBANException("; ".join(mistakes))
+    return True
 
 
 for user in users:
