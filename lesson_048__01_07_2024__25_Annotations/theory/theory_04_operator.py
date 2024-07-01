4381400ab975d98567f0e6bee8ed705c36f55766
ibarbylev <ibarbylev@gmail.com>
2024-07-01 11:38:14 +0300

    some fixes

--- lesson_048__01_07_2024__25_Annotations/theory/theory_04_operator.py
+++ lesson_048__01_07_2024__25_Annotations/theory/theory_04_operator.py
@@ -4,3 +4,10 @@ print(add(5, 3))        # Результат: 8
 print(sub(10, 2))       # Результат: 8
 print(mul(2, 4))        # Результат: 8
 print(truediv(16, 2))   # Результат: 8.0
+
+
+iter_value = map(lambda x, y: x + y, 'abc', 'cdef')
+# iter_value = map(add, 'abc', 'cdef')
+
+for item in iter_value:
+    print(item)
