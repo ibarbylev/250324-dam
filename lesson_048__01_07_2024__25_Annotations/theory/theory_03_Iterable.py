0ac5e963cfda6c705a4ce85a0a5c759f7fd94c05
ibarbylev <ibarbylev@gmail.com>
2024-07-01 11:21:25 +0300

    some fixes

--- lesson_048__01_07_2024__25_Annotations/theory/theory_03_Iterable.py
+++ lesson_048__01_07_2024__25_Annotations/theory/theory_03_Iterable.py
@@ -4,3 +4,13 @@ from typing import Iterable
 def generate_numbers(n: int) -> Iterable[int]:
     for i in range(n):
         yield i
+
+
+def generate_numbers_2(n: int) -> Iterable[str]:
+    for char in ['a', 'b', 'b']:
+        yield char
+
+
+g = generate_numbers(10)
+for item in g:
+    print(item)
