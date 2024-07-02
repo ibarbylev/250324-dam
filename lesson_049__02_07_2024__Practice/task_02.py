319628db17a69a1a41d78c1b24a3fa7eacd878f2
ibarbylev <ibarbylev@gmail.com>
2024-07-02 12:23:21 +0300

    some fixes

--- lesson_049__02_07_2024__Practice/task_02.py
+++ lesson_049__02_07_2024__Practice/task_02.py
@@ -14,14 +14,29 @@ https://stackoverflow.com/questions/27264250/how-to-annotate-a-generator-in-pyth
 Generator[yield_type, send_type, return_type]
 """
 
-from typing import Generator
+from typing import Generator, Iterator
 
 
-def gen_7_symbols():
-    pass
+def gen_7_symbols() -> Generator[str, str, None]:
+    str_out = " "
+    while True:
+        str_in = yield str_out
 
+        # ===== variant 1 ==========
+        # if len(str_in) > 7:
+        #     str_out = str_in[:7]
+        # else:
+        #     yield str_in.rjust(7, '0')
+
+        # ===== variant 2 ==========
+        str_out = f'{str_in[:7]:>07}'
+
+
+g = gen_7_symbols()
+next(g)
 
 
 print(g.send('123456789'))  # 1234567
 print(g.send('12345'))      # 0012345
+
 
