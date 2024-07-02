e74faafa632d38a7f84b336e83616bce644cfc25
ibarbylev <ibarbylev@gmail.com>
2024-07-02 12:46:06 +0300

    some fixes

--- lesson_049__02_07_2024__Practice/task_03.py
+++ lesson_049__02_07_2024__Practice/task_03.py
@@ -5,14 +5,19 @@
     - суб-генератор возвращает имена из файла,
     - а основной генератор в нужный момент считывает и возвращает значения из суб-генератора.
 """
+from typing import Iterator
 
 
-def gen_read_file():
-    pass
+def gen_read_file() -> Iterator[str]:
+    with open("names.txt", encoding='utf-8') as file:
+        for line in file:
+            yield line.strip()
 
 
-def gen_main():
-    pass
+def gen_main() -> Iterator[str]:
+    yield "Привет"
+    yield from gen_read_file()
+    yield "и добро пожаловать!"
 
 
 print(f"{', '.join([x for x in gen_main()])}")
