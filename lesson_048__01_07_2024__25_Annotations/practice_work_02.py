ce8a5764e4071e7da4c35ff4a7b835459c53ecb0
ibarbylev <ibarbylev@gmail.com>
2024-07-02 13:12:38 +0300

    some fixes

--- lesson_048__01_07_2024__25_Annotations/practice_work_02.py
+++ lesson_048__01_07_2024__25_Annotations/practice_work_02.py
@@ -3,10 +3,13 @@
  - и возвращает наибольшую строку из списка.
 Функция должна быть аннотирована с помощью аннотаций типов.
 """
+from typing import List
 
 
-def get_largest_string():
-    pass
+def get_largest_string(strings: List[str]) -> str:
+    if not strings:
+        return ""
+    return max(strings, key=len)
 
 
 lst = ["apple", "banana", "pear", "grape", "orange"]
