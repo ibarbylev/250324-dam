34531f4ec1e7f4ca553682454189e5b34b116880
ibarbylev <ibarbylev@gmail.com>
2024-07-02 13:04:12 +0300

    some fixes

--- lesson_049__02_07_2024__Practice/task_04.py
+++ lesson_049__02_07_2024__Practice/task_04.py
@@ -5,15 +5,25 @@
 файловый ресурс корректно освобождается (использования исключения GeneratorExit и очистку ресурсов).
 """
 import json
-from typing import Iterator
+from typing import Generator, Dict
 
 
-def gen_clients():
-    pass
+def gen_clients() -> Generator[Dict[str, str], None, None]:
+    try:
+        with open("users.json", encoding='utf-8') as f:
+            lst = json.load(f)
+            for i in lst:
+                yield i
+    except GeneratorExit as e:
+        print(f"GeneratorExit, {e}")
+    finally:
+        f.close()
+        print("close")
 
 
 gen = gen_clients()
 for client in gen:
     print(client)
 gen.close()
+
 
