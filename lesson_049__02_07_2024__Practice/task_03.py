00533bf29eb6d3fee27d6016d6b3ba66a5057dad
ibarbylev <ibarbylev@gmail.com>
2024-07-02 12:47:36 +0300

    some fixes

--- lesson_049__02_07_2024__Practice/task_03.py
+++ lesson_049__02_07_2024__Practice/task_03.py
@@ -8,17 +8,17 @@
 from typing import Iterator
 
 
-def gen_read_file() -> Iterator[str]:
-    with open("names.txt", encoding='utf-8') as file:
+def gen_read_file(filename) -> Iterator[str]:
+    with open(filename, encoding='utf-8') as file:
         for line in file:
             yield line.strip()
 
 
-def gen_main() -> Iterator[str]:
+def gen_main(filename) -> Iterator[str]:
     yield "Привет"
-    yield from gen_read_file()
+    yield from gen_read_file(filename)
     yield "и добро пожаловать!"
 
 
-print(f"{', '.join([x for x in gen_main()])}")
+print(f"{', '.join([x for x in gen_main("names.txt")])}")
 # Привет, Alice, John, Max, Maria, и добро пожаловать!
