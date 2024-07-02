38ce6c6e26ade64b37c6f3009e29755b79dda3f6
ibarbylev <ibarbylev@gmail.com>
2024-07-02 11:58:48 +0300

    some fixes

--- lesson_049__02_07_2024__Practice/task_01.py
+++ lesson_049__02_07_2024__Practice/task_01.py
@@ -4,7 +4,13 @@
 from typing import Iterator
 
 
-def gen_double_num():
-    pass
+def gen_double_num(start: int = 1) -> Iterator[int]:
+    num = start
+    while True:
+        yield num
+        num *= 2
 
 
+gen = gen_double_num()
+for _ in range(10):
+    print(next(gen))
