ae1ec49e0651065b052abe8fa63a406fc669dbb0
ibarbylev <ibarbylev@gmail.com>
2024-07-05 11:12:10 +0300

    some fixes

--- lesson_051__05_07_2024__Summary/task_05_2_explanation_for_a_dict.py
+++ lesson_051__05_07_2024__Summary/task_05_2_explanation_for_a_dict.py
@@ -6,8 +6,11 @@ from operator import itemgetter
 a_dict = {'a': 1, 'c': 1, 'b': 5}
 
 for k, v in groupby(a_dict.items(), itemgetter(1)):
-    print(k, list(v))
+    print(k, dict(v))
 
 print(a_dict.items())
-# 1 [('a', 1), ('c', 1)]
-# 5 [('b', 5)]
+# 1 {'a': 1, 'c': 1}
+# 5 {'b': 5}
+
+# [ (1, {'a': 1, 'c': 1}) , (5, {'b': 5})   ]
+# [ 1: {'a': 1, 'c': 1} , 5: {'b': 5})   ]
