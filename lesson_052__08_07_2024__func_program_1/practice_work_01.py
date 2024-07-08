0fa798f50dfd8f2ab30f8c7b60d2f62e1fa4c69f
ibarbylev <ibarbylev@gmail.com>
2024-07-08 10:51:15 +0300

    some fixes

--- lesson_052__08_07_2024__func_program_1/practice_work_01.py
+++ lesson_052__08_07_2024__func_program_1/practice_work_01.py
@@ -3,9 +3,16 @@
 """
 
 
-def more_than_value(nums: list[int | float], value: int | float) -> bool:
-    pass
+from typing import List, Union
+
+
+def more_than_value(nums: List[Union[int, float]], value: Union[int, float]) -> bool:
+    for i in nums:
+        if i <= value:
+            return False
+    return True
 
 
 lst = [2, 9, 4, 1, 12]
 print(more_than_value(lst, 1))
+print(more_than_value(lst, 0))
