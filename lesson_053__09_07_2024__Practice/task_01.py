ba08a752dc67d0a9b9bb71557624d3af6096fc16
ibarbylev <ibarbylev@gmail.com>
2024-07-09 11:58:59 +0300

    some fixes,

--- lesson_053__09_07_2024__Practice/task_01.py
+++ lesson_053__09_07_2024__Practice/task_01.py
@@ -4,11 +4,13 @@
 Использовать lambda-функции и filter.
 При описании функции использовать аннотации.
 """
+from typing import Iterator, List
 
 
-def filter_long_words(word_list):
-    pass
-
+def filter_long_words(word_list: List[str]) -> Iterator[str]:
+    return filter(lambda word: len(word) > 3, word_list)
+    # return filter(lambda x: len(x) > 3, word_list)
+    # return filter(lambda word: len(word)>3, word_list)
 
 # Пример использования функции
 words = ["cat", "elephant", "dog", "tiger", "ant", "monkey", "bee", "fox"]
