563496dd4e0e4e53886425a2061af5f91c5a16d2
ibarbylev <ibarbylev@gmail.com>
2024-07-09 12:37:28 +0300

    some fixes,

--- lesson_053__09_07_2024__Practice/task_04.py
+++ lesson_053__09_07_2024__Practice/task_04.py
@@ -8,25 +8,29 @@
 которая возвращает итератор.
 Соответственно, адаптированная функция
 """
+from typing import Iterator, List
 import json
 
 
-def write_file(filename, words):
-    pass
+def write_file(filename: str, words: List[str]) -> None:
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(words, f, indent=4)
 
 
-def read_file(filename):
-    pass
+def read_file(filename: str) -> Iterator[str]:
+    with open(filename, 'r', encoding='utf-8') as f:
+        return iter(json.load(f))
 
 
-def filter_long_words(words):
-    pass
+def filter_long_words(word_list: Iterator[str]) -> Iterator[str]:
+    return filter(lambda word: len(word) > 3, word_list)
 
 
 words = ["cat", "elephant", "dog", "tiger", "ant", "monkey", "bee", "fox"]
 write_file('words.json', words)
 
 words_iter = read_file('words.json')
+# print(type(words_iter))
 filtered_words = filter_long_words(words_iter)
 print(*filtered_words)
 # elephant tiger monkey
