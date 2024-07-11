622fa46407ba4de57ad784fa7e3d1b8c266323ac
ibarbylev <ibarbylev@gmail.com>
2024-07-11 10:31:11 +0300

    some fixes

--- lesson_054__11_07_2024__28_func_program_2/theory/theory_03_comprehension_as_map_and_filter.py
+++ lesson_054__11_07_2024__28_func_program_2/theory/theory_03_comprehension_as_map_and_filter.py
@@ -5,10 +5,10 @@ list или tuple compression
 nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 # Convert numbers to strings using list compression
-print(...)
+print([str(n) for n in nums])
 
 # Get only even items using list compression
-print([...])
+print([n for n in nums if n % 2 == 0])
 
 # Get only odd items using generator expression (tuple compression)
-print(*(...))
+print(*(n for n in nums if n % 2 != 0))
