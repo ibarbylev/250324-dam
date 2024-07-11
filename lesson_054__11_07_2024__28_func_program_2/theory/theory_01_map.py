710a7b4a6fbbe046c7aa4c0c05046dc9b5b20566
ibarbylev <ibarbylev@gmail.com>
2024-07-11 10:39:15 +0300

    some fixes

--- lesson_054__11_07_2024__28_func_program_2/theory/theory_01_map.py
+++ lesson_054__11_07_2024__28_func_program_2/theory/theory_01_map.py
@@ -1,8 +1,12 @@
+from typing import Iterator
+
 nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 # Convert int to string
 map_iter = map(str, nums)
 print(map_iter)  # <map object at 0x7f7edb9de560>
+print(isinstance(map_iter, Iterator))
+print(type(map_iter))
 print(list(map_iter))
 # ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
