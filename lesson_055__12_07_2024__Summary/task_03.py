fa0dd1c9a360fbd2ec00b32600d7b6e2e6bc2466
ibarbylev <ibarbylev@gmail.com>
2024-07-12 10:46:36 +0300

    some fixes

--- lesson_055__12_07_2024__Summary/task_03.py
+++ lesson_055__12_07_2024__Summary/task_03.py
@@ -18,6 +18,6 @@ list_2 = ['a', 'b', 'c', 'd', 'e', 'f']
 list_3 = [10, 20, 30, 40]
 data = [list_1, list_2, list_3]
 
-new_list = ...
+new_list = [f"{a}_{b}_{c}" for a, b, c in zip_longest(*data, fillvalue=0)]
 print(*new_list, sep='  ')
 # 1_a_10  2_b_20  3_c_30  4_d_40  0_e_0  0_f_0
