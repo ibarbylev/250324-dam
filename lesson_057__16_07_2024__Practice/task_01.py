1d0b6e9345996b9d1be93cbc2d27d62fa113ddf3
ibarbylev <ibarbylev@gmail.com>
2024-07-16 10:18:54 +0300

    initial commit

--- lesson_057__16_07_2024__Practice/task_01.py
+++ lesson_057__16_07_2024__Practice/task_01.py
@@ -2,6 +2,16 @@
 
 
 class Car:
+    def __init__(self, model, year, color):
+        self.model = model
+        self.year = year
+        self.color = color
+
     def __str__(self):
-        pass
+        return f"{self.model} {self.year} {self.color}"
+
+
+if __name__ == '__main__':
+    car = Car("a", "1", "c")
+    print(car)
 
