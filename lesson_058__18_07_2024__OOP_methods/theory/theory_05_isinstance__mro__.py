9316a79dbee1bda8831e0badf330e94906c59753
ibarbylev <ibarbylev@gmail.com>
2024-07-18 11:26:31 +0300

    some fixes

--- lesson_058__18_07_2024__OOP_methods/theory/theory_05_isinstance__mro__.py
+++ lesson_058__18_07_2024__OOP_methods/theory/theory_05_isinstance__mro__.py
@@ -9,9 +9,10 @@ class MySubClass(MyClass):
 my_object = MyClass()
 my_sub_object = MySubClass()
 
-print(isinstance(my_object, MyClass))        # True
-print(isinstance(my_sub_object, MyClass))    # True
-print(isinstance(my_object, MySubClass))     # False
+print(isinstance(my_object, MyClass))          # True
+print(isinstance(my_sub_object, MyClass))      # True
+print(isinstance(my_sub_object, MySubClass))   # True
+print(isinstance(my_object, MySubClass))       # False
 
 """__mro__ - Method Resolution Order (порядок разрешения методов). 
 Он используется для определения порядка, в котором 
@@ -22,3 +23,5 @@ print(my_object.__class__.__mro__)
 # (<class '__main__.MyClass'>, <class 'object'>)
 print(MySubClass.__mro__)
 # (<class '__main__.MySubClass'>, <class '__main__.MyClass'>, <class 'object'>)
+print(dir(MySubClass))
+# ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getstate__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__']
