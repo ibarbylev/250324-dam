00ff808d58292869be8c4ac8b4cd2a2472793b08
ibarbylev <ibarbylev@gmail.com>
2024-07-25 10:31:42 +0300

    some fixes

--- lesson_062__25_07_2024__dunder_methods/total_recall/task_01.py
+++ lesson_062__25_07_2024__dunder_methods/total_recall/task_01.py
@@ -16,6 +16,23 @@ import time
 import requests
 
 
+def param_decorator(param):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            total_time = 0
+            for i in range(param):
+                print(f"Iteration {i+1}/{param}")
+                start = time.time()
+                func(*args, **kwargs)
+                end = time.time()
+                delay = end - start
+                print(f"Delay is {delay:.2f} seconds")
+                total_time += delay
+            average_time = total_time / param
+            print(f"Average time executing {func.__name__}:  {average_time:.2f} seconds")
+        return wrapper
+    return decorator
+
 
 @param_decorator(5)
 def get_request(url):
