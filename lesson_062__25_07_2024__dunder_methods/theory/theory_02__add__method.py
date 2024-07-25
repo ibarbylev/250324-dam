e1a646bbc71387b848e194265d237d5d421e677d
ibarbylev <ibarbylev@gmail.com>
2024-07-25 10:56:55 +0300

    some fixes

--- lesson_062__25_07_2024__dunder_methods/theory/theory_02__add__method.py
+++ lesson_062__25_07_2024__dunder_methods/theory/theory_02__add__method.py
@@ -3,7 +3,10 @@ class Money:
         self.amount = amount
         self.currency = currency
 
-
+    def __add__(self, other):
+        if self.currency == other.currency:
+            return Money(self.amount + other.amount, self.currency)
+        raise ValueError("Cannot add amounts with different currencies")
 
     def __str__(self):
         return f"{self.amount} {self.currency}"
