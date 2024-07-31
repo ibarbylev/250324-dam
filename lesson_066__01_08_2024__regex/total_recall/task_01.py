1b7499e038f66d624711336622203671cd1cd196
ibarbylev <ibarbylev@gmail.com>
2024-08-01 01:56:50 +0300

    some fixes

---
+++ lesson_066__01_08_2024__regex/total_recall/task_01.py
@@ -0,0 +1,51 @@
+"""Создайте класс Book с приватными атрибутами (__private)
+    author (автор)
+    title (название)
+Реализуйте доступ к этим атрибутам через геттеры и сеттеры.
+
+Далее необходимо создать метод
+    - create_book(title, author), который принимает название книги и её автора
+    и возвращает экземпляр класса Book.
+
+Не забудьте также реализовать метод __str__
+"""
+
+
+class Book:
+    def __init__(self, title, author):
+        self.__title = title
+        self.__author = author
+
+    @property
+    def title(self):
+        return self.__title
+
+    @title.setter
+    def title(self, author):
+        self.__author = author
+
+    @property
+    def author(self):
+        return self.__title
+
+    @author.setter
+    def author(self, author):
+        self.__author = author
+
+    @classmethod
+    def create_book(cls, title, author):
+        return cls(title, author)
+
+    def __str__(self):
+        return f'Book "{self.__title}" by {self.__author}'
+
+
+if __name__ == '__main__':
+    book1 = Book.create_book("Leo Tolstoy", "War and Peace")
+    book2 = Book.create_book("Jane Austen", "Pride and Prejudice")
+
+    print(book1)
+    print(book2)
+
+# Book War and Peace by Leo Tolstoy
+# Book Pride and Prejudice by Jane Austen
---
+++ lesson_066__01_08_2024__regex/total_recall/task_02.py
@@ -0,0 +1,51 @@
+"""Реализуйте класс библиотеки Library, с атрибутом
+    - shelf: список, представляющий полку библиотеки
+и методами
+    - add_book: добавляет новую книгу на полку библиотеки, используя метод Book.create.
+
+Также необходимо создать (переопределить) некоторые dunder методы (см.примеры вывода)
+"""
+
+
+from task_01 import Book
+
+
+class Library:
+    def __init__(self):
+        self.shelf = []
+
+    def add_book(self, title, author):
+        new_book = Book.create_book(title, author)
+        self.shelf.append(new_book)
+
+    def __getitem__(self, index):
+        books = self.shelf[index]
+        if isinstance(index, slice):
+            return [str(book) for book in books]
+        return books
+
+    def __str__(self):
+        books = "\n".join([f" - {str(b)}" for b in self.shelf])
+        return f"The total number of books in the library is {len(self.shelf)}:\n{books}"
+
+
+library = Library()
+
+library.add_book("War and Peace", "Leo Tolstoy")
+library.add_book("Pride and Prejudice", "Jane Austen")
+library.add_book("Ten Little Niggers", "Agatha Christie")
+library.add_book("Gone with the Wind", "Margaret Mitchell")
+
+
+print(library[:2])
+# ['Book War and Peace by Leo Tolstoy', 'Book Pride and Prejudice by Jane Austen']
+
+print(library[0])
+# Book War and Peace by Leo Tolstoy
+
+print(library)
+# The total number of books in the library is 3:
+#  - Book "War and Peace" by Leo Tolstoy
+#  - Book "Pride and Prejudice" by Jane Austen
+#  - Book "Ten Little Niggers" by Agatha Christie
+#  - Book "Gone with the Wind" by Margaret Mitchell
