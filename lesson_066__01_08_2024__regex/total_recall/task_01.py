556d65edab67f03cfc2f6b9bba3a0c7181c2edaf
ibarbylev <ibarbylev@gmail.com>
2024-08-01 10:31:44 +0300

    some fixes

--- lesson_066__01_08_2024__regex/total_recall/task_01.py
+++ lesson_066__01_08_2024__regex/total_recall/task_01.py
@@ -12,7 +12,32 @@
 
 
 class Book:
-    pass
+    def __init__(self, title, author):
+        self.__title = title
+        self.__author = author
+
+    @property
+    def title(self):
+        return self.__title
+
+    @title.setter
+    def title(self, title):
+        self.__title = title
+
+    @property
+    def author(self):
+        return self.__author
+
+    @author.setter
+    def author(self, author):
+        self.__author = author
+
+    @classmethod
+    def create_book(cls, title, author):
+        return cls(title, author)
+
+    def __str__(self):
+        return f'Book "{self.__title}" by {self.__author}'
 
 
 if __name__ == '__main__':
