9dbf93f2aadf623ffd9beacf32d47792410a9c16
ibarbylev <ibarbylev@gmail.com>
2024-08-08 10:22:22 +0300

    some fixes

--- lesson_070__08_08_2024__parsing/total_recall/task_01.py
+++ lesson_070__08_08_2024__parsing/total_recall/task_01.py
@@ -7,13 +7,19 @@ import re
 import requests
 
 URL = "https://en.wikipedia.org/wiki/REST"
+# [a-zA-Z]+
 
-...
-uniq_words = 0
-if status_code == 200:
-    pass
-
-
+response = requests.get(URL)
+# uniq_words = 0
+if response.status_code == 200:
+    html = response.text
+    pattern = r'[a-zA-Z]+'
+    words: list = re.findall(pattern, html)
+    # print(len(words))
+    uniq_words = set(words)
+    # print(len(uniq_words))
+else:
+    print("Not available!")
 
 print(f"There are {len(uniq_words)} unique words on the {URL} website.")
 #  There are 1702 unique words on the https://en.wikipedia.org/wiki/REST website.
