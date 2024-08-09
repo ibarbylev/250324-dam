3a6c35fcf980059eb55afc2e9d1b5acd9933376b
ibarbylev <ibarbylev@gmail.com>
2024-08-09 03:38:19 +0300

    some fixes

--- lesson_070__08_08_2024__parsing/homework/homework_01.py
+++ lesson_070__08_08_2024__parsing/homework/homework_01.py
@@ -3,27 +3,27 @@
     - использует библиотеку Beautiful Soup для парсинга HTML
     - и выводит список всех ссылок на странице.
 """
+from typing import List
 
 import requests
 from bs4 import BeautifulSoup
 
 
-def get_links(url):
+def get_links(url: str) -> List[str]:
     response = requests.get(url)
 
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
-        links = [
-            a['href'] for a in soup.find_all('a')
-            if a['href'][:4] == 'http'
-        ]
+        links = [link['href'] for link in soup.find_all('a', href=True)]
         return links
+
     else:
         print(f"Ошибка при выполнении запроса: {response.status_code}")
         return []
 
 
 # url = input("Введите URL-адрес веб-страницы: ")
-url = "https://en.wikipedia.org/wiki/Main_Page"
+url = 'https://ru.wikipedia.org/wiki/Wi-Fi'
+
 links = get_links(url)
 print(*links, sep='\n')
--- lesson_070__08_08_2024__parsing/homework/homework_02.py
+++ lesson_070__08_08_2024__parsing/homework/homework_02.py
@@ -9,28 +9,28 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_headers(url, header_tag):
+def get_headers(url, tag=None) -> None:
+    if tag is None:
+        tags = [f'h{i}' for i in range(1, 7)]
+    else:
+        tags = [tag]
     response = requests.get(url)
 
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
-        headers = soup.find_all(header_tag)
-        return headers
+
+        for tag in tags:
+            headers = soup.find_all(tag)
+            print(f'=============== Headers <{tag}> ================:')
+            for header in headers:
+                print(f'     {header.text.strip()}')
     else:
         print(f"Ошибка при выполнении запроса: {response.status_code}")
-        return []
 
 
 # url = input("Введите URL-адрес веб-страницы: ")
 # header_tag = input("Введите уровень заголовка (например, h1, h2, h3): ")
 
-url = "https://en.wikipedia.org/wiki/Main_Page"
-header_tag = "h2"
-headers = get_headers(url, header_tag)
+url = 'https://ru.wikipedia.org/wiki/Wi-Fi'
+get_headers(url)
 
-if headers:
-    print(f"Заголовки уровня {header_tag}, найденные на странице:")
-    for header in headers:
-        print(header.text.strip())
-else:
-    print("Заголовков не найдено.")
