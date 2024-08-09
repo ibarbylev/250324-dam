20dd4d12459f0d80ee47e143f50c24b905b26e81
ibarbylev <ibarbylev@gmail.com>
2024-08-09 10:07:41 +0300

    some fixes

---
+++ lesson_071__09_08_2024__Summary/task_03.py
@@ -0,0 +1,11 @@
+from local_settings import HOST, USER, PASSWORD, DATABASE
+
+dbconfig = {
+    'host': HOST,
+    'user': USER,
+    'password': PASSWORD,
+    'database': DATABASE
+}
+
+
+print(dbconfig)
