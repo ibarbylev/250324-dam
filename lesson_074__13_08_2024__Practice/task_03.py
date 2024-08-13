b4ba385162b76ea95eebca3eca75ff292ba900ab
ibarbylev <ibarbylev@gmail.com>
2024-08-13 15:05:40 +0300

    some fixes

--- lesson_074__13_08_2024__Practice/task_03.py
+++ lesson_074__13_08_2024__Practice/task_03.py
@@ -5,19 +5,30 @@ import requests
 from bs4 import BeautifulSoup
 
 URL = 'https://www.stroysa.tomsk.ru/catalog/derevyannyy_pogonazh/'
-response = requests.get(URL)
-html = response.text
-
-soup = BeautifulSoup(html, 'html.parser')
-cards = soup.find_all('div', class_='catalog-item-info')
-
 urls = []
 
-card = cards[0]
-first_tag_a = card.find('a', href=True)
-print(first_tag_a)
-url = f'https://www.stroysa.tomsk.ru{first_tag_a.get('href')}'
-print(url)
+for i in range(1, 1000):
+    if i == 1:
+        url_page = URL
+    else:
+        url_page = URL + f'?PAGEN_2={i}'
+
+    print(f'?PAGEN_2={i}')
+
+    response = requests.get(url_page)
+    if response.status_code != 200:
+        break
+
+    html = response.text
+
+    soup = BeautifulSoup(html, 'html.parser')
+    cards = soup.find_all('div', class_='catalog-item-info')
+
+    for card in cards:
+        first_tag_a = card.find('a', href=True)
+        url = f'https://www.stroysa.tomsk.ru{first_tag_a.get('href')}'
+        urls.append(url)
+        print(url)
 
 # catalog-item-info
 
