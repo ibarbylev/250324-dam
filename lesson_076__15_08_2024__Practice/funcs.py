39becd8a9fd99825de200f3885d26345bc6de647
ibarbylev <ibarbylev@gmail.com>
2024-08-15 13:00:20 +0300

    some fixes

--- lesson_076__15_08_2024__Practice/funcs.py
+++ lesson_076__15_08_2024__Practice/funcs.py
@@ -9,12 +9,42 @@ def show_cats(cursor) -> None:
 def show_movies(cursor, num_cat: str) -> None:
     query = f"""
         SELECT 
-            f.title, f.release_year, f.description, c.film_id
+            f.title,
+            f.release_year,
+            f.description,
+            c.film_id,
+            GROUP_CONCAT(DISTINCT name.last_name
+                SEPARATOR ', ') actors
         FROM
             sakila.film f
                 LEFT JOIN
-            sakila.film_category c ON c.film_id = {num_cat} LIMIT 10;
+            sakila.film_category c ON c.film_id = {num_cat}
+                LEFT JOIN
+            sakila.film_actor a ON a.film_id = c.film_id
+                LEFT JOIN
+            sakila.actor name ON name.actor_id = a.actor_id
+        GROUP BY name.last_name
+        LIMIT 10;
     """
     cursor.execute(query)
     movies = cursor.fetchall()
     print(*movies, sep='\n')
+
+
+# SELECT
+#     f.title,
+#     f.release_year,
+#     f.description,
+#     c.film_id,
+#     GROUP_CONCAT(DISTINCT name.last_name
+#         SEPARATOR ', ') actors
+# FROM
+#     sakila.film f
+#         LEFT JOIN
+#     sakila.film_category c ON c.film_id = f.film_id
+#         LEFT JOIN
+#     sakila.film_actor a ON a.film_id = c.film_id
+#         LEFT JOIN
+#     sakila.actor name ON name.actor_id = a.actor_id
+# GROUP BY name.last_name
+# LIMIT 10
